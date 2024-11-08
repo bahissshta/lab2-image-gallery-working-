a656ceda8377642b727c24462022cd7661b5cfaa
bahissshta <grewalbahishta@gmail.com>
2024-11-07 18:04:13 -0700

    its working!

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("sheep_button").addEventListener("click", function() {
+    document.getElementById("sheep").src = "images/sheep.webp";
+});
+
+document.getElementById("bear").addEventListener("click", function() {
+    document.getElementById("sheep").src = "images/bear.webp";
+});
+
+document.getElementById("moose").addEventListener("click", function() {
+    document.getElementById("sheep").src = "images/moose.webp";
+});
+
+document.getElementById("wolf").addEventListener("click", function() {
+    document.getElementById("sheep").src = "images/wolf.webp";
+});
+
+document.getElementById("elk").addEventListener("click", function() {
+    document.getElementById("sheep").src = "images/elk.webp";
+});
